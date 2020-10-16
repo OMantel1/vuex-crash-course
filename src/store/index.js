ff801264ced827ab5a790b1226ca5ec8d5226241
omantel <oliviermantel@gmail.com>
2020-10-16 14:54:16 +0200

    Add fetchTodos feature - fetch api in vue store - load todos fetched in Todo component

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import Axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,30 +6,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    todos: [{
-        "userId": 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false
-      }
+    todos: [
+      // {
+      //   "userId": 1,
+      //   "id": 1,
+      //   "title": "delectus aut autem",
+      //   "completed": false
+      // },
+      // {
+      //   "userId": 1,
+      //   "id": 2,
+      //   "title": "quis ut nam facilis et officia qui",
+      //   "completed": false
+      // },
+      // {
+      //   "userId": 1,
+      //   "id": 3,
+      //   "title": "fugiat veniam minus",
+      //   "completed": false
+      // }
     ]
   },
   getters: {
     allTodos: (state) => state.todos
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setTodos: (state, todos) => (state.todos = todos)
+  },
+  actions: {
+    async fetchTodos({ commit }) {
+      const response = await Axios.get('http://jsonplaceholder.typicode.com/todos')
+    commit('setTodos', response.data)
+    }
+  },
   modules: {}
 })
